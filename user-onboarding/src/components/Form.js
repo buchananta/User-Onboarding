e16e636a5baf5eef065cc2f2cc29d7d8287f1b3c
Trevor Buchanan <trevor@trevorbuchanan.com>
2020-08-19 15:12:24 -0500

    got basic yum form checking setup

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import axios from 'axios'; 
+import * as yup from 'yup';
+import formSchema from '../validation/formSchema';
 //skeleton for various data
 const defaultFormData = {
   'username': '',
@@ -12,12 +14,36 @@ const defaultFormData = {
 
 function Form({inputChange}) {
   const [formData, setFormData] = useState(defaultFormData);
+  const [formErrors, setFormErrors] = useState([]);
   const [disabled, setDisabled] = useState(true);
 
   const onInputChange = (event) => {
     const {name, value} = event.target;
+    yup
+      .reach(formSchema, name)
+      //we can then run validate using the value
+      .validate(value)
+      // if the validation is successful, we can clear the error message
+      .then(valid => {
+        setFormErrors({
+          ...formErrors,
+          [name]: ""
+        });
+      })
+      /* if the validation is unsuccessful, we can set the error message to the message 
+        returned from yup (that we created in our schema) */
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0]
+        });
+      });
     setFormData({...formData, [name]: value})
   }
+
+  formSchema.isValid(formData).then(valid => {
+    setDisabled(!valid);
+  })
 
   const submitData = (event) => {
     event.preventDefault();
@@ -40,7 +66,7 @@ function Form({inputChange}) {
           onChange={onInputChange}
         />
       </label>
-      <button disabled={false} onClick={submitData} >Submit</button>
+      <button disabled={disabled} onClick={submitData} >Submit</button>
     </form>
   )
 }
---
+++ user-onboarding/src/validation/formSchema.js
@@ -0,0 +1,22 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3, "Username must be at least 3 characters long.")
+    .required("Username is Required"),
+  // email: yup
+  //   .string()
+  //   .email("Must be a valid email address.")
+  //   .required("Must include email address."),
+  // password: yup
+  //   .string()
+  //   .min(8, "Password must be at least 8 characters long.")
+  //   .required("Password is required")
+  // tos: yup
+  //   .string()
+  //   .oneOf(['true'], 'you must accept the terms of service.')
+    // required isn't required for checkboxes.
+});
+
+export default formSchema;
